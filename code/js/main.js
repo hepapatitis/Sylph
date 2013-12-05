ed44b93c4e4bda337f235ea05a778087d0307aee
hepapatitis <hepapatitis@yahoo.com>
2013-12-05 17:10:27 +1100

    Changed top class after going off .first-page

--- code/js/main.js
+++ code/js/main.js
@@ -21,10 +21,22 @@ $(function() {
     $(window).scroll(function(){
 		scrollTop = $(this).scrollTop();
 		
-		  // Animate
-		if (scrollTop < $("#features").scrollTop()) {
-			$('#features').css("visibility", "display");
+		// Change the color of nav bar
+		if (scrollTop < ($(".first-page").height())) {
+			if($("#top-nav").hasClass("nav-white-container"))
+			{
+				$("#top-nav").removeClass("nav-white-container");
+				$("#top-nav").addClass("nav-white-transparent");
+			}
 		}
+		else {
+			if($("#top-nav").hasClass("nav-white-transparent"))
+			{
+				$("#top-nav").removeClass("nav-white-transparent");
+				$("#top-nav").addClass("nav-white-container");
+			}
+		}
+		
     });
 	
 	$(window).resize(function () {
