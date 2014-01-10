30235c987693aa0d0ec46d8e5bc5d9145151dc30
hepapatitis <hepapatitis@yahoo.com>
2014-01-10 19:26:15 +1100

    Finale

--- code/js/main.js
+++ code/js/main.js
@@ -49,4 +49,16 @@ $(function() {
 	$(window).resize(function () {
 		resize_first_page();
 	});
+	
+	// Nice Scroll for #
+	$("a").click(function() {
+		var elementClicked = $(this).attr("href");
+		var destination = $(elementClicked).offset().top;
+		
+		var extramargin = 0;
+		if(elementClicked != "#top")
+			extramargin -= 50;
+		$("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination+extramargin}, 500 );
+		return false;
+	});
 });
