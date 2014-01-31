2bc995cb4cf4fc37757f7139f34cb89ec8414d25
hepapatitis <hepapatitis@yahoo.com>
2014-01-31 16:57:53 +1100

    Parallax update & several items removal
    Removed unused files
    Updated parallax effect correctly
    New image for Quote BG

--- code/js/main.js
+++ code/js/main.js
@@ -1,4 +1,5 @@
-function resize_first_page()
+// Auto Resize First Column if you want an resize function for static background
+function resize_first_column()
 {
 	$('.first-page').css('min-height', $( window ).height());
 }
@@ -7,9 +8,37 @@ $('.carousel').carousel({
 	interval: 5500
 })
 
-$(function() {
-	resize_first_page();
 
+var revapi;
+
+$(window).resize(function () {
+	// Auto Resize First Column on window resize
+	//resize_first_column();
+});
+	
+$(function() {
+	// Auto Resize First Column
+	//resize_first_column();
+
+	// NiceScroll JS
+	//$("html").niceScroll();
+	
+	// FancyBox JS
+	$(".fancybox").fancybox();
+	
+	// Slider Revolution
+	revapi = jQuery('.tp-banner').revolution(
+	{
+		delay:15000,
+		startwidth:1170,
+		startheight:500,
+		hideThumbs:10,
+		fullWidth:"off",
+		fullScreen:"on",
+		fullScreenOffsetContainer: ""
+	});
+
+	// Auto Change CSS when scrolling above slider
     var scrollTop = $(window).scrollTop();
  
     $(window).scroll(function(){
@@ -36,10 +65,6 @@ $(function() {
 	// Mix It Up Plugin
 	$('#Grid').mixitup();
 	
-	$(window).resize(function () {
-		resize_first_page();
-	});
-	
 	// Nice Scroll for #
 	$("a").click(function() {
 		var elementClicked = $(this).attr("href");
@@ -64,4 +89,20 @@ $(function() {
 			}
 		});
 	});
+	
+	// Perform Parallax Animation
+	$('[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+		
+		$(window).scroll(function() {
+            var yPos = -($(window).scrollTop() / $bgobj.data('speed')); 
+             
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+			
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    }); 
+ 
 });
